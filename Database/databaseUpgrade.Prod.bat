c216999c1658f294eff9fc8e48db7c02e25f5515
NickSavino <nicksavino2@gmail.com>
2024-11-10 01:29:38 -0700

    fdas

--- Database/databaseUpgrade.Prod.bat
+++ Database/databaseUpgrade.Prod.bat
@@ -6,8 +6,8 @@ set SERVER_NAME=%SERVER_NAME%
 set DATABASE_NAME=%DATABASE_NAME%
 set USERNAME=%USERNAME%
 set PASSWORD=%PASSWORD%
-set SCRIPTS_DIR=%CD%\Database\Upgrade
-set LOG_FILE=%CD%\Database\sql_scripts_log.txt
+set SCRIPTS_DIR=%CD%\Upgrade
+set LOG_FILE=%CD%\sql_scripts_log.txt
 
 REM Clear the screen
 cls
