e7979dabf6e92a31a8d1ead443d9aac34d320091
Sunny Parmar <sunnyparmar@hotmail.ca>
2024-11-10 00:36:07 -0700

    add mapbox

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import Mapbox from '@rnmapbox/maps';
+import Mapbox, { MapView } from '@rnmapbox/maps';
 
-Mapbox.setAccessToken('<YOUR_ACCESSTOKEN>');
-
-
-
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    height: 300,
-    width: 300,
-  },
-  map: {
-    flex: 1
-  }
-});
-
+Mapbox.setAccessToken('');
 
 export default function Index() {
   return (
     <View style={styles.page}>
         <View style={styles.container}>
-          <Mapbox.MapView style={styles.map} />
+          <Mapbox.MapView styleURL={Mapbox.StyleURL.Dark}/>
         </View>
       </View>
   );
 }
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+
+    justifyContent: 'center',
+
+    alignItems: 'center',
+  },
+
+  container: {
+    height: '100%',
+
+    width: '100%',
+  },
+
+  map: {
+    flex: 1,
+  },
+});
