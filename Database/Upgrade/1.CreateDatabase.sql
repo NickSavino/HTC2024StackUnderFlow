8049d9ec706fa6bae60d5877720a0fbbe12e3d53
NickSavino <nicksavino2@gmail.com>
2024-11-10 01:18:13 -0700

    daf

--- Database/Upgrade/1.CreateDatabase.sql
+++ Database/Upgrade/1.CreateDatabase.sql
@@ -5,10 +5,14 @@ GO
 -- Check if the database exists
 IF DB_ID('HTC_DB') IS NOT NULL
 BEGIN
+    PRINT 'Database HTC_DB exists. Dropping it now...';
+
     -- Set the database to SINGLE_USER mode to drop active connections
     ALTER DATABASE HTC_DB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+    -- Drop the database
     DROP DATABASE HTC_DB;
-    PRINT 'Database HTC_DB dropped.';
+    PRINT 'Database HTC_DB dropped successfully.';
 END
 ELSE
 BEGIN
@@ -17,10 +21,22 @@ END
 GO
 
 -- Create the database
+PRINT 'Creating database HTC_DB...';
 CREATE DATABASE HTC_DB;
-PRINT 'Database HTC_DB created.';
+PRINT 'Database HTC_DB created successfully.';
 GO
 
 -- Switch to the new database
 USE HTC_DB;
+GO
+
+-- Optional: Check if the USE statement worked
+IF DB_NAME() = 'HTC_DB'
+BEGIN
+    PRINT 'Switched to HTC_DB successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'Failed to switch to HTC_DB.';
+END
 GO
