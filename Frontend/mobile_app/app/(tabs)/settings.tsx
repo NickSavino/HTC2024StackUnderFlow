ff7afdac9503fbe79af37f22727a691d2a956190
NickSavino <nicksavino2@gmail.com>
2024-11-17 18:33:46 -0700

    recreated web app with typescript

---
+++ Frontend/mobile_app/app/(tabs)/settings.tsx
@@ -0,0 +1,11 @@
+import { ThemedView } from "@/components/ThemedView"
+
+
+const SettingsScreen = () => {
+
+    return (
+        <ThemedView></ThemedView>
+    )
+}
+
+export default SettingsScreen;
